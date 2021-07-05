b28bf87047d65d72152f867dda9a190478e41ce5
Ihor Chernikov <chernikov.ihor@gmail.com>
2021-07-05 12:50:27 +0300

    task solved

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,106 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void AgedBrieQualityMaxCheck(){
+        Item[] items = new Item[]{new Item("Aged Brie", 0,0 )};
+        GildedRose app = new GildedRose(items);
+
+        for(int i =0;i<60;i++){
+            app.updateQuality();
+        }
+
+        assertEquals(-60, app.items[0].sellIn);
+        assertEquals(50,app.items[0].quality);
+    }
+
+    @Test
+    void SulfurasQualityMaxCheck(){
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 0,80 )};
+        GildedRose app = new GildedRose(items);
+
+        for(int i =0;i<100;i++){
+            app.updateQuality();
+        }
+
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(80,app.items[0].quality);
+    }
+
+    @Test
+    void notSpecialItemLessThanZero(){
+        Item[] items = new Item[]{new Item("Elixir of the Mongoose", 2,30 )};
+        GildedRose app = new GildedRose(items);
+
+        for(int i =0;i<4;i++){
+            app.updateQuality();
+        }
+        assertEquals(-2, app.items[0].sellIn);
+        assertEquals(24,app.items[0].quality);
+    }
+
+    @Test
+    void QualityIncreaseBrie(){
+        Item[] items = new Item[]{new Item("Aged Brie", 5,30 )};
+        GildedRose app = new GildedRose(items);
+
+        for(int i =0;i<2;i++){
+            app.updateQuality();
+        }
+        assertEquals(3, app.items[0].sellIn);
+        assertEquals(32,app.items[0].quality);
+    }
+
+    @Test
+    void BackstageZeroQualityAfterSellInDate(){
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 2,20 )};
+        GildedRose app = new GildedRose(items);
+
+        for(int i =0;i<4;i++){
+            app.updateQuality();
+        }
+
+        assertEquals(-2, app.items[0].sellIn);
+        assertEquals(0,app.items[0].quality);
+    }
+
+    @Test
+    void BackstageQualityIncreaseLessThanTenDays(){
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",10,30)};
+        GildedRose app = new GildedRose(items);
+
+        for(int i =0;i<2;i++){
+            app.updateQuality();
+        }
+        assertEquals(8, app.items[0].sellIn);
+        assertEquals(34,app.items[0].quality);
+    }
+
+    @Test
+    void BackstageQualityIncreaseLessThanFiveDays(){
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",5,30)};
+        GildedRose app = new GildedRose(items);
+
+        for(int i =0;i<2;i++){
+            app.updateQuality();
+        }
+        assertEquals(3, app.items[0].sellIn);
+        assertEquals(36,app.items[0].quality);
+    }
+
+    @Test
+    void checkConjured() {
+        Item[] items = new Item[]{new Item("Conjured", 10, 20)};
+        GildedRose app = new GildedRose(items);
+
+        for (int i = 0; i < 2; i++) {
+            app.updateQuality();
+        }
+
+        assertEquals(8, app.items[0].sellIn);
+        assertEquals(16, app.items[0].quality);
+    }
 }
--- Java/src/test/java/com/gildedrose/TexttestFixture.java
+++ Java/src/test/java/com/gildedrose/TexttestFixture.java
@@ -2,6 +2,9 @@ package com.gildedrose;
 
 public class TexttestFixture {
     public static void main(String[] args) {
+
+        int days = 2;
+
         System.out.println("OMGHAI!");
 
         Item[] items = new Item[] {
@@ -17,11 +20,6 @@ public class TexttestFixture {
                 new Item("Conjured Mana Cake", 3, 6) };
 
         GildedRose app = new GildedRose(items);
-
-        int days = 2;
-        if (args.length > 0) {
-            days = Integer.parseInt(args[0]) + 1;
-        }
 
         for (int i = 0; i < days; i++) {
             System.out.println("-------- day " + i + " --------");
